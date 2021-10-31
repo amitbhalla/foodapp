35e35c4dee73e58c97039e039233e3d78afc0a51
Amit Bhalla <amit9815@gmail.com>
2021-10-31 19:13:25 +0530

    Meals

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Fragment } from 'react';
 
 import './App.css';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
